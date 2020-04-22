03748035f3f3edc5cc085f4c553d980e5cea7e12
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-22 15:15:26 +0200

    esercizio da correggere

---
+++ main.js
@@ -0,0 +1,11 @@
+
+for (var i=0; i < 101; i++)  {
+    console.log(i);
+    if (i % 3 == 0) {
+        console.log('Fizz');
+    } else if (i % 5 == 0) {
+        console.log('Buzz');
+    } else (i % 15 == 0)
+        console.log('FizzBuzz');
+
+}
