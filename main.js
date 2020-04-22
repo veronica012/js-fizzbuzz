f21d9089e8d77dc9c81777ffcc4e81f2ef08fe8c
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-22 15:31:23 +0200

    ok

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
 for (var i=0; i < 101; i++)  {
-    console.log(i);
-    if (i % 3 == 0) {
-        console.log('Fizz');
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
     } else if (i % 5 == 0) {
         console.log('Buzz');
-    } else (i % 15 == 0)
-        console.log('FizzBuzz');
-
+    } else if (i % 3 == 0) {
+        console.log('Fizz');
+    } else {
+        console.log (i);
+    }
 }
